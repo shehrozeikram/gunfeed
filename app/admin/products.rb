e24710b87d9d25867ce54bec1d62248c61823c2d
shehroze ikram <shehrozeikram2@gmail.com>
2022-11-09 20:29:48 +0500

    active admin is implimented

---
+++ app/admin/products.rb
@@ -0,0 +1,18 @@
+ActiveAdmin.register Product do
+
+  # See permitted parameters documentation:
+  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
+  #
+  # Uncomment all parameters which should be permitted for assignment
+  #
+  permit_params :product_url, :title, :price, :store, :amount, :category, :caliber, :brand, :mpn, :upc, :body, :user_id, :image
+  #
+  # or
+  #
+  # permit_params do
+  #   permitted = [:product_url, :title, :price, :store, :amount, :category, :caliber, :brand, :mpn, :upc, :body, :user_id]
+  #   permitted << :other if params[:action] == 'create' && current_user.admin?
+  #   permitted
+  # end
+  
+end
