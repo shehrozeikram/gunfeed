1fc1bef24da2dd6c6c8739d52a9e330a51ddcae5
shehroze ikram <shehrozeikram2@gmail.com>
2022-12-08 14:50:37 +0500

    show page of store is fixed

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,5 +1,14 @@
 class StoresController < ApplicationController
   def index
+    @categories = Category.all
     @stores = Store.all
   end
+
+
+  def show
+    @store = Store.find(params[:id])
+    @products = Product.where(store_id: params[:id] )
+    @categories = Category.all
+  end
+
 end
