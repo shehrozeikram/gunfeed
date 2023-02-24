cefb3612a083905c8fd3675bd39d4d72034097ce
shehrozeikram <shehrozeikram2@gmail.com>
2023-02-24 15:55:56 +0500

    minor fix

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -1,4 +1,6 @@
 class StoresController < ApplicationController
+
+  before_action :categories, :coupons, :rebates, :comments
   def index
     @categories = Category.all
     @stores = Store.all
@@ -8,6 +10,22 @@ class StoresController < ApplicationController
   def show
     @store = Store.find(params[:id])
     @products = Product.where(store_id: params[:id] )
+    @categories = Category.all
+  end
+
+  def coupons
+    @coupons = Coupon.all
+  end
+
+  def comments
+    @comments = Comment.all
+  end
+
+  def rebates
+    @rebates = Rebate.all
+  end
+
+  def categories
     @categories = Category.all
   end
 
